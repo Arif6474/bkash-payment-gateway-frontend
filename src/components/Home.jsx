3011d8124e0494134a11b1f03d86aba55e9e95b0
Arif Islam <arifulislam64743@gmail.com>
2024-01-20 21:24:01 +0600

    integrateBkash

---
+++ src/components/Home.jsx
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+const Home = () => {
+
+    const pay = async () => {
+        try {
+            const { data } = await axios.post('http://localhost:5000/api/bkash/payment/create', { amount: 50, orderId: 1 }, { withCredentials: true })
+            window.location.href = data.bkashURL
+        } catch (error) {
+            console.log(error.response.data)
+        }
+    }
+    return (
+        <div>
+            <button onClick={pay}>Pay bkash</button>
+        </div>
+    )
+}
+
+export default Home
